b09df0ace746dd38e35a0c32cd110348252024b3
Chris Joel <0xcda7a@gmail.com>
2022-11-03 09:39:36 -0700

    chore: Remove vestigial comments

--- rust/noosphere/src/builder.rs
+++ rust/noosphere/src/builder.rs
@@ -7,10 +7,3 @@ use noosphere_core::{authority::Authorization, view::Sphere};
 use noosphere_fs::SphereFs;
 use noosphere_storage::{db::SphereDb, interface::Store};
 use ucan::crypto::KeyMaterial;
-
-
-
-
-// pub struct NoosphereBuilder<'a, K, S> {
-//   user_key: Option<K>
-// }
