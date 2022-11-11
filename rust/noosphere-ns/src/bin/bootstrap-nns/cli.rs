8bc1e17e51f9da8e49615383b890ad8cc6b2c7a2
Jordan Santell <jordan@jsantell.com>
2022-11-11 09:08:05 -0800

    dev: First pass on a `bootstrap` CLI in noosphere-ns to spin up DHT bootstrap nodes for the name system.

---
+++ rust/noosphere-ns/src/bin/bootstrap-nns/cli.rs
@@ -0,0 +1,50 @@
+#![cfg(not(target_arch = "wasm32"))]
+
+use clap::{Parser, Subcommand};
+use serde::Deserialize;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(name = "bootstrap_nns")]
+pub struct CLI {
+    #[clap(subcommand)]
+    pub command: CLICommand,
+}
+
+#[derive(Subcommand)]
+pub enum CLICommand {
+    /// Run DHT nodes in the Noosphere Name System.
+    Run {
+        /// The path to the bootstrap configuration, a TOML file, containing
+        /// entries for keyname/port pairs.
+        #[clap(short, long)]
+        config: Option<PathBuf>,
+
+        /// If no configuration path provided, the name of the Noosphere keypair to use
+        /// stored in `~/.noosphere/keys/`.
+        #[clap(short, long)]
+        key: Option<String>,
+
+        /// If no configuration path provided, the listening port of this DHT node.
+        #[clap(short, long)]
+        port: Option<u16>,
+    },
+
+    /// Utility to create keys compatible with Noosphere.
+    KeyGen {
+        /// The name of the key to be stored in `~/.noosphere/keys/`.
+        #[clap(short, long)]
+        key: String,
+    },
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CLIConfigNode {
+    pub key: String,
+    pub port: Option<u16>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CLIConfig {
+    pub nodes: Vec<CLIConfigNode>,
+}
---
+++ rust/noosphere-ns/src/bin/bootstrap-nns/utils.rs
@@ -0,0 +1,62 @@
+use anyhow::{anyhow, Result};
+use home;
+use noosphere::key::{InsecureKeyStorage, KeyStorage};
+use noosphere_ns::Multiaddr;
+use std::future::Future;
+use std::path::PathBuf;
+use ucan_key_support::ed25519::Ed25519KeyMaterial;
+
+/// Runs `future` until a Ctrl+C signal is received either during
+/// `future`'s execution, or after, or if `future` returns an `Err`.
+pub async fn run_until_abort(future: impl Future<Output = Result<()>>) -> Result<()> {
+    // Allow aborting (ctrl+c) during the initial run,
+    // and after (when we want to wait exclusively for ctrl+c signal)
+    let mut aborted = false;
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => { aborted = true; },
+        result = future => { let _ = result?; }
+    };
+    if !aborted {
+        tokio::signal::ctrl_c().await?;
+    }
+    Ok(())
+}
+
+pub async fn get_key_material(
+    key_storage: &InsecureKeyStorage,
+    key_name: &str,
+) -> Result<Ed25519KeyMaterial> {
+    if let Some(km) = key_storage.read_key(key_name).await?.take() {
+        Ok(km)
+    } else {
+        Err(anyhow!(
+            "No key \"{}\" found in `~/.noosphere/keys/`.",
+            key_name
+        ))
+    }
+}
+
+pub fn create_listening_address(port: u16) -> Multiaddr {
+    format!("/ip4/127.0.0.1/tcp/{}", port)
+        .parse()
+        .expect("parseable")
+}
+
+pub fn get_keys_dir() -> Result<PathBuf> {
+    Ok(home::home_dir()
+        .ok_or_else(|| anyhow!("Could not discover home directory."))?
+        .join(".noosphere"))
+}
+
+pub fn filter_bootstrap_peers(self_address: &Multiaddr, peers: &[Multiaddr]) -> Vec<Multiaddr> {
+    peers
+        .iter()
+        .filter_map(|addr| {
+            if addr != self_address {
+                Some(addr.to_owned())
+            } else {
+                None
+            }
+        })
+        .collect()
+}
