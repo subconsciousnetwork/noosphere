ba83ffe1468b92cf7e971e019926b39ee2582fe1
Jordan Santell <jsantell@users.noreply.github.com>
2023-02-20 09:38:27 -0800

    feat: Ephemeral keys in `orb-ns` image, bind api operator on 0.0.0.0 (#234)

--- images/orb-ns/README.md
+++ images/orb-ns/README.md
@@ -3,9 +3,12 @@
 cd noosphere/
 
 # Build
-podman build -t orb-ns:latest -f images/orb-ns/orb-ns.Dockerfile .
+podman build -t orb-ns:latest -f images/orb-ns/Dockerfile .
 
 # Run
 podman run -it -v ~/.noosphere:/home/dhtuser/.noosphere -t orb-ns:latest bootstrap-key 6666 6667
+
+# Run with ephemeral key
+podman run -it -t orb-ns:latest ephemeral 6666 6667
 ```
 
--- images/orb-ns/start.sh
+++ images/orb-ns/start.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 MOUNT_DIR="/home/dhtuser/.noosphere"
+
+KEY=$1
+API_PORT=$3
+SWARM_PORT=6666
+if [[ "$2" ]]; then
+	SWARM_PORT="$2"
+fi
+
+cd /home/dhtuser
+
+if [[ "$KEY" = "ephemeral" ]]; then
+	# This has a side effect of ensuring a ~/.noosphere/keys directory
+	# exists to store the key, circumventing the need for mounting 
+	orb-ns key-gen --key ephemeral
+fi
 if [[ ! -d "$MOUNT_DIR" ]]; then
 	echo "Missing mount on $MOUNT_DIR."
 	exit 1
@@ -10,18 +25,11 @@ if [[ -z "$1" ]]; then
 	exit 1
 fi
 
-KEY=$1
-API_PORT=$3
-SWARM_PORT=6666
-if [[ "$2" ]]; then
-	SWARM_PORT="$2"
-fi
 LISTENING_ADDRESS="0.0.0.0:${SWARM_PORT}"
 
-cd /home/dhtuser
-
 if [[ "$API_PORT" ]]; then
-	orb-ns run --key ${KEY} --listening-address ${LISTENING_ADDRESS} --api-address ${API_PORT}
+	API_ADDRESS="0.0.0.0:${API_PORT}"
+	orb-ns run --key ${KEY} --listening-address ${LISTENING_ADDRESS} --api-address ${API_ADDRESS}
 else
 	orb-ns run --key ${KEY} --listening-address ${LISTENING_ADDRESS}
 fi
