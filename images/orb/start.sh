36509109e00cec625c23de625cee9654582a9f8f
Jordan Santell <jsantell@users.noreply.github.com>
2023-03-16 08:21:58 -0700

    feat: pass along a name resolver API in orb image (#275)
    Also, make docker scripts executable and verbose for debugging.

--- images/orb/start.sh
+++ images/orb/start.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+set -x
 
 KEY=$1
 COUNTERPART=$2
 IPFS_API=$3
-
-echo "STARTING IPFS NODE @ ${IPFS_API}"
+NS_API=$4
 
 cd /root/sphere
 
 orb key create $KEY
 
 if ! [ -d "./.sphere" ]; then
-  orb sphere create --owner-key $KEY
+	orb sphere create --owner-key $KEY
 fi
 
 orb config set counterpart $COUNTERPART
-orb serve -i 0.0.0.0 --ipfs-api $IPFS_API
+
+ARGS="-i 0.0.0.0"
+ARGS="${ARGS} --ipfs-api ${IPFS_API}"
+
+if ! [ -z "$4" ]; then
+	ARGS="${ARGS} --name-resolver-api ${NS_API}"
+fi
+
+orb serve $ARGS
