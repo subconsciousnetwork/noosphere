faaadf5a24218cdf1a45e85b92b7e645606b3735
Christopher Joel <240083+cdata@users.noreply.github.com>
2023-04-04 11:30:23 -0700

    chore: Revise default logging (#299)

--- rust/noosphere-core/src/tracing.rs
+++ rust/noosphere-core/src/tracing.rs
@@ -19,8 +19,7 @@ pub fn initialize_tracing() {
     INITIALIZE_TRACING.call_once(|| {
         tracing_subscriber::registry()
             .with(tracing_subscriber::EnvFilter::new(
-                std::env::var("RUST_LOG")
-                    .unwrap_or_else(|_| "noosphere_cli,orb,tower_http=debug".into()),
+                std::env::var("RUST_LOG").unwrap_or_else(|_| "".into()),
             ))
             .with(tracing_subscriber::fmt::layer())
             .init();
