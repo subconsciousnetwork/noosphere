454147bfa95f74d2f3140576bee21c77b6ca388c
Chris Joel <0xcda7a@gmail.com>
2023-05-31 10:20:09 -0700

    feat!: Update Package.swift for Noosphere v0.11.0-pre.2

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
             path: "swift/Sources/SwiftNoosphere"),
         .binaryTarget(
             name: "LibNoosphere",
-            url: "https://github.com/subconsciousnetwork/noosphere/releases/download/noosphere-v0.10.11/libnoosphere-apple-xcframework.zip",
-            checksum: "0cf8f95e7442ed9649b40d98c67416647496963f66538d102f9c3246021f0335"),
+            url: "https://github.com/subconsciousnetwork/noosphere/releases/download/noosphere-v0.11.0-pre.2/libnoosphere-apple-xcframework.zip",
+            checksum: "fc595bfccd52c64427731ef664b298ab63348377f42d64d6ee167595adb3d5f3"),
         .testTarget(
             name: "SwiftNoosphereTests",
             dependencies: ["SwiftNoosphere"],
