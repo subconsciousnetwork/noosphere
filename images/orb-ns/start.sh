23ada3e4575a1c6cb52cdfbf16e67e098a296993
Jordan Santell <jsantell@users.noreply.github.com>
2024-04-16 12:32:21 -0700

    feat: Parameterize the orb-ns Docker image so that ENV can be used to customize more arguments. (#889)

--- images/orb-ns/start.sh
+++ images/orb-ns/start.sh
@@ -2,31 +2,28 @@
 set -x
 
 MOUNT_DIR="/home/dhtuser/.noosphere"
-
-KEY=$1
-CONFIG_FILE=$2
+ORB_NS="/usr/bin/orb-ns"
 
 cd /home/dhtuser
 
-if [[ "$KEY" = "ephemeral" ]]; then
+if [[ "$KEY_NAME" = "ephemeral" ]]; then
 	# This has a side effect of ensuring a ~/.noosphere/keys directory
 	# exists to store the key, circumventing the need for mounting 
-	orb-ns key-gen --key ephemeral
+	$ORB_NS key-gen --key ephemeral
 fi
 if [[ ! -d "$MOUNT_DIR" ]]; then
 	echo "Missing mount on $MOUNT_DIR."
 	exit 1
 fi
-if [[ -z "$1" ]]; then
-	echo "ARGS: KEY CONFIG_FILE"	
-	exit 1
-fi
-if [[ -z "$2" ]]; then
-	echo "Missing config file path."
-	exit 1
-fi
 
 echo "RUST_LOG=${RUST_LOG}"
 echo "NOOSPHERE_LOG=${NOOSPHERE_LOG}"
+echo "KEY_NAME=${KEY_NAME}"
+echo "P2P_ADDRESS=${P2P_ADDRESS}"
+echo "API_ADDRESS=${API_ADDRESS}"
+echo "IPFS_URL=${IPFS_URL}"
 
-orb-ns run --config $CONFIG_FILE
+$ORB_NS run --key "${KEY_NAME}" \
+	--listening-address "${P2P_ADDRESS}" \
+	--api-address "${API_ADDRESS}" \
+	--ipfs-api-url "${IPFS_URL}"
